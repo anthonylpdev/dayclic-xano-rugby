8d70b15db9e9e09e0b5951686b00d1bc5980d7e7
Anthony Lopez <anthonylopez@MacBook-Pro-de-Anthony.local>
2023-09-19 09:18:57 +0200

    Base settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      "background/primary": "#0f0f12",
+      "background/secondary": "#19191f",
+      "background/hover": "#24242d",
+      "background/accent": "#2d3cff",
+      "content/primary": "#fcfcfc",
+      "content/secondary": "#a1a1a1",
+      "border/secondary": "#25252e",
+      "border/primary": "#47474c",
+    },
+    fontSize: {
+      base: "1rem",
+      lg: "1.5rem",
+    },
+    fontFamily: {
+      roboto: "Roboto",
+    },
+    borderRadius: {
+      none: "0",
+      xs: "0.3125rem",
+      sm: "1rem",
+      default: "5rem",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
